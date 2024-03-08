90326e2fdd1fc2124dceec69a28b9c3dfcdfb258
Ambuj <ambuj.s@devtrust.biz>
2024-03-09 04:59:17 +0530

    change

--- src/app/(private)/dashboard/layout.tsx
+++ src/app/(private)/dashboard/layout.tsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/navigation";
 import React, { ReactNode, useEffect } from "react";
 const Layout: React.FC<{ children: ReactNode }> = ({ children }) => {
   const router = useRouter();
-  const { isLogin:isLoggedIn } = useAuth();
+  const { isLogin: isLoggedIn } = useAuth();
   console.log(isLoggedIn, "islogin");
   useEffect(() => {
     if (!isLoggedIn) {
