8ac684be7bdbdb934565e9bb8a67499fcb9efaba
ambuj <ambuj1264675@gmail.com>
2024-03-20 09:59:18 +0530

    chan ger

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -13,10 +13,13 @@ const endpointSecret = process.env.STRIPE_ENDPOINTSECRET!;
 export async function POST(req: NextRequest) {
   const rawBody = await req.text();
 
-  const sig:any =  headers().get("Stripe-Signature") as string;
+  const sig: any = headers().get("Stripe-Signature") as string;
   try {
-
-    const event = await stripe.webhooks.constructEvent(rawBody, sig, endpointSecret);
+    const event = await stripe.webhooks.constructEvent(
+      rawBody,
+      sig,
+      endpointSecret
+    );
     switch (event.type) {
       case "checkout.session.async_payment_failed":
         break;
@@ -24,53 +27,59 @@ export async function POST(req: NextRequest) {
         break;
       case "checkout.session.completed":
         const session: Session = event.data.object as Session;
-        stripe.customers.retrieve(session.customer).then(async (customerResponse) => {
-          const customer = customerResponse as Stripe.Customer;
-          const customerId = customer.metadata.userId;
-          const billingDate = session.created;
-          const formattedBillingDate = new Date(billingDate * 1000).toISOString();
-          const inputDate = new Date(formattedBillingDate);
-          const findUser = await Subscribes.findOne({
-            userId: customerId,
-          });
-          if (!findUser) {
-            const updateUserPayment = await Subscribes.create({
+        stripe.customers
+          .retrieve(session.customer)
+          .then(async (customerResponse) => {
+            const customer = customerResponse as Stripe.Customer;
+            const customerId = customer.metadata.userId;
+            const billingDate = session.created;
+            const formattedBillingDate = new Date(
+              billingDate * 1000
+            ).toISOString();
+            const inputDate = new Date(formattedBillingDate);
+            const findUser = await Subscribes.findOne({
               userId: customerId,
-              billingDate: inputDate,
-              paid: true,
-              approved: true,
-              amount: session.amount_total / 100,
             });
-            await updateUserPayment.save();
-            if (updateUserPayment) {
-              await Users.findOneAndUpdate(
-                {
-                  _id: customerId,
-                },
-                {
-                  subscribed: true,
-                  approved: true,
-                },
-                {
-                  new:true
-                }
-              );
-            }
-          } else {
-            const updatedSub = await Subscribes.findOneAndUpdate(
-              {
-                userId: customerId,
-              },
-              {
+            console.log(findUser, "findUSer");
+            if (!findUser) {
+              const updateUserPayment = await Subscribes.create({
                 userId: customerId,
                 billingDate: inputDate,
                 paid: true,
                 approved: true,
                 amount: session.amount_total / 100,
+              });
+              await updateUserPayment.save();
+              if (updateUserPayment) {
+                await Users.findOneAndUpdate(
+                  {
+                    _id: customerId,
+                  },
+                  {
+                    subscribed: true,
+                    approved: true,
+                  },
+                  {
+                    new: true,
+                  }
+                );
               }
-            );
-            if (updatedSub) {
-              await Users.findOneAndUpdate(
+            } else {
+              const updatedSub = await Subscribes.findOneAndUpdate(
+                {
+                  userId: customerId,
+                },
+                {
+                  userId: customerId,
+                  billingDate: inputDate,
+                  paid: true,
+                  approved: true,
+                  amount: session.amount_total / 100,
+                }
+              );
+              console.log(updatedSub, "updatedSub");
+
+              const resultForUpdate = await Users.findOneAndUpdate(
                 {
                   _id: customerId,
                 },
@@ -79,13 +88,12 @@ export async function POST(req: NextRequest) {
                   approved: true,
                 },
                 {
-                  new:true
+                  new: true,
                 }
               );
+              console.log(resultForUpdate, "resultForUpdate");
             }
-          }
-        });
-
+          });
 
         // Then define and call a function to handle the event checkout.session.completed
         break;
