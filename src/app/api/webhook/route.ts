662edf024c2bbec0f46459afa1f902a065a83212
ambuj <ambuj1264675@gmail.com>
2024-03-20 18:15:17 +0530

    chnages

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -10,97 +10,34 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
 });
 
 const endpointSecret = process.env.STRIPE_ENDPOINTSECRET!;
+
 export async function POST(req: NextRequest) {
   const rawBody = await req.text();
 
   const sig: any = headers().get("Stripe-Signature") as string;
+  console.log(sig, "sig");
   try {
     const event = await stripe.webhooks.constructEvent(
       rawBody,
       sig,
       endpointSecret
     );
+    console.log(event, "event")
     switch (event.type) {
       case "checkout.session.async_payment_failed":
         break;
       case "checkout.session.async_payment_succeeded":
         break;
       case "checkout.session.completed":
-        const session: Session = event.data.object as Session;
-        stripe.customers
-          .retrieve(session.customer)
-          .then(async (customerResponse) => {
-            const customer = customerResponse as Stripe.Customer;
-            const customerId = customer.metadata.userId;
-            const billingDate = session.created;
-            const formattedBillingDate = new Date(
-              billingDate * 1000
-            ).toISOString();
-            const inputDate = new Date(formattedBillingDate);
-            const findUser = await Subscribes.findOne({
-              userId: customerId,
-            });
-            console.log(findUser, "findUSer");
-            if (!findUser) {
-              const updateUserPayment = await Subscribes.create({
-                userId: customerId,
-                billingDate: inputDate,
-                paid: true,
-                approved: true,
-                amount: session.amount_total / 100,
-              });
-              await updateUserPayment.save();
-              if (updateUserPayment) {
-                await Users.findOneAndUpdate(
-                  {
-                    _id: customerId,
-                  },
-                  {
-                    subscribed: true,
-                    approved: true,
-                  },
-                  {
-                    new: true,
-                  }
-                );
-              }
-            } else {
-              const updatedSub = await Subscribes.findOneAndUpdate(
-                {
-                  userId: customerId,
-                },
-                {
-                  userId: customerId,
-                  billingDate: inputDate,
-                  paid: true,
-                  approved: true,
-                  amount: session.amount_total / 100,
-                }
-              );
-              console.log(updatedSub, "updatedSub");
-
-              const resultForUpdate = await Users.findOneAndUpdate(
-                {
-                  _id: customerId,
-                },
-                {
-                  subscribed: true,
-                  approved: true,
-                },
-                {
-                  new: true,
-                }
-              );
-              console.log(resultForUpdate, "resultForUpdate");
-            }
-          });
-
-        // Then define and call a function to handle the event checkout.session.completed
+        await handleCheckoutSessionCompleted(event.data.object as Session);
+        break;
+      case "payment_intent.succeeded":
+        await handlePaymentIntentSucceeded(event.data.object as any);
         break;
       case "checkout.session.expired":
         break;
       default:
-        console.log(`Unhandled event type ${event.type}`);
+        console.log(`Unhandled event`);
         break;
     }
     return NextResponse.json({
@@ -114,5 +51,103 @@ export async function POST(req: NextRequest) {
       success: false,
       message: "Webhook Error: " + err.message,
     });
+  }
+}
+
+async function handleCheckoutSessionCompleted(session: Session) {
+  try {
+    const customer :any= await stripe.customers.retrieve(session.customer);
+    const customerId = customer?.metadata?.userId;
+    const billingDate = session.created;
+    const formattedBillingDate = new Date(billingDate * 1000).toISOString();
+    const inputDate = new Date(formattedBillingDate);
+
+    const findUser = await Subscribes.findOne({ userId: customerId });
+    if (!findUser) {
+      const updateUserPayment = await Subscribes.create({
+        userId: customerId,
+        billingDate: inputDate,
+        paid: true,
+        approved: true,
+        amount: session.amount_total / 100,
+      });
+      await updateUserPayment.save();
+      if (updateUserPayment) {
+        await Users.findOneAndUpdate(
+          { _id: customerId },
+          { subscribed: true, approved: true },
+          { new: true }
+        );
+      }
+    } else {
+      const updatedSub = await Subscribes.findOneAndUpdate(
+        { userId: customerId },
+        {
+          userId: customerId,
+          billingDate: inputDate,
+          paid: true,
+          approved: true,
+          amount: session.amount_total / 100,
+        }
+      );
+
+      const resultForUpdate = await Users.findOneAndUpdate(
+        { _id: customerId },
+        { subscribed: true, approved: true },
+        { new: true }
+      );
+    }
+  } catch (error) {
+    console.error("Error handling checkout session completed:", error);
+    throw error;
+  }
+}
+
+async function handlePaymentIntentSucceeded(session: Session) {
+  try {
+    const customer :any= await stripe.customers.retrieve(session.customer);
+    const customerId = customer?.metadata.userId;
+    const billingDate = session.created;
+    const formattedBillingDate = new Date(billingDate * 1000).toISOString();
+    const inputDate = new Date(formattedBillingDate);
+
+    const findUser = await Subscribes.findOne({ userId: customerId });
+    if (!findUser) {
+      const updateUserPayment = await Subscribes.create({
+        userId: customerId,
+        billingDate: inputDate,
+        paid: true,
+        approved: true,
+        amount: session.amount_total / 100,
+      });
+      await updateUserPayment.save();
+      if (updateUserPayment) {
+        await Users.findOneAndUpdate(
+          { _id: customerId },
+          { subscribed: true, approved: true },
+          { new: true }
+        );
+      }
+    } else {
+      const updatedSub = await Subscribes.findOneAndUpdate(
+        { userId: customerId },
+        {
+          userId: customerId,
+          billingDate: inputDate,
+          paid: true,
+          approved: true,
+          amount: session.amount_total / 100,
+        }
+      );
+
+      const resultForUpdate = await Users.findOneAndUpdate(
+        { _id: customerId },
+        { subscribed: true, approved: true },
+        { new: true }
+      );
+    }
+  } catch (error) {
+    console.error("Error handling payment intent succeeded:", error);
+    throw error;
   }
 }
