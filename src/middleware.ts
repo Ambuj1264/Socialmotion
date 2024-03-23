9e48f83f6f7649b69fa31602f73afa335934d11d
ambuj <ambuj1264675@gmail.com>
2024-03-23 21:27:34 +0530

    chnages

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,14 @@
+import  cors  from "cors";
 export { default } from "next-auth/middleware";
+
+const corsOptions = {
+  origin: 'your-frontend-origin', // Adjust origin as needed
+  methods: 'GET, POST, PUT, DELETE, OPTIONS', // Adjust methods as needed
+  allowedHeaders: ['Content-Type', 'Authorization'], // Adjust headers as needed
+};
+
 export const config = {
   matcher: ["/dashboard", "/buy", "/buynow/:id", "/paymentsuccess","/paymentunsuccess","/product", "/demoDetails/:id"],
 };
+
+export const corsMiddleware = cors(corsOptions);
