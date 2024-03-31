1fe2611f303b61b8530dae4dd3454357a8a6a681
ambuj <ambuj1264675@gmail.com>
2024-03-31 10:58:32 +0530

    chnage

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,7 @@
-import  cors  from "cors";
-import { NextResponse } from "next/server";
+
 export { default } from "next-auth/middleware";
 
 
 export const config = {
-  matcher: ["/dashboard", "/buy", "/buynow/:id", "/paymentsuccess","/paymentunsuccess","/product", "/demoDetails/:id", "/api/:path*" ],
+  matcher: ["/dashboard", "/buy", "/buynow/:id", "/paymentsuccess","/paymentunsuccess","/product", "/demoDetails/:id" ],
 };
-export function middleware() {
-  // retrieve the current response
-  const res = NextResponse.next()
-
-  // add the CORS headers to the response
-  res.headers.append('Access-Control-Allow-Credentials', "true")
-  res.headers.append('Access-Control-Allow-Origin', '*') // replace this your actual origin
-  res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
-  res.headers.append(
-      'Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-
-  return res
-}
