1906161f81d844df63797e2f5247b1bf5f84ea8f
ambuj <ambuj1264675@gmail.com>
2024-04-12 15:15:04 +0530

    changes

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,5 +3,5 @@ export { default } from "next-auth/middleware";
 
 
 export const config = {
-  matcher: [ "/buy", "/buynow/:id", "/paymentsuccess","/paymentunsuccess", ],
+  matcher: [ "/buy", "/buynow/**", "/paymentsuccess","/paymentunsuccess", ],
 };
